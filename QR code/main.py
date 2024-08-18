5fce35aa091f204ebcc47547cb2a06266816d1ef
Dev Mistry <90182112+DevM15@users.noreply.github.com>
2024-08-18 13:03:36 +0530

    Update main.py

--- QR code/main.py
+++ QR code/main.py
@@ -2,8 +2,10 @@ import qrcode
 from pyzbar.pyzbar import decode
 from PIL import Image
 
+// Qr code generator
 myqr = qrcode.make("www.youtube.com/watch?v=IUQVO97zcE0")
 myqr.save("")
 
+// Qr code reader
 b = decode(Image.open("myqr.png"))
 print(b[0].data.decode("ascii"))
